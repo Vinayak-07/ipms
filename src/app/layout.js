e581bdd38cbf67776982d0e4b83c85ca59d23697
Vinayak-07 <vinayaknavlekar07@gmail.com>
2026-03-17 20:26:25 +0530

    feat: initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
